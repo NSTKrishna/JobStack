8e1f41b0c878ffcaa4dfcf39032faa5e1cb90eb0
NSTKrishna <krishnagehlot936@gmail.com>
2025-11-16 16:12:15 +0530

    Update cors.Middleware.js

--- Server/middlewares/cors.Middleware.js
+++ Server/middlewares/cors.Middleware.js
@@ -1,9 +1,7 @@
 const cors = require("cors");
 
 const corsMiddleware = cors({
-  origin: `https://job-stack-teal.vercel.app`,
-  methods: ["GET", "POST", "PUT", "DELETE"],
-  credentials: true,
+  origin: "*",
 });
 
 module.exports = {
